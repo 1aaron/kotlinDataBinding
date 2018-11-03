0432cd2287d47af75592ac86559dfe0d423bc5be
Developer <1aaron>
2018-11-03 18:22:42 -0500

    added 2 way binding

--- app/src/main/java/com/example/aaron/twodatabinding/observables/ExampleObservable.kt
+++ app/src/main/java/com/example/aaron/twodatabinding/observables/ExampleObservable.kt
@@ -6,4 +6,13 @@ class ExampleObservable(val setTitle: Boolean): BaseObservable() {
     //no need of @Bindable
     var oneWayText: String? = null
         get() = if (setTitle) "ONE WAY BINDING" else ""
+    var twoWayModifiedText: String? = "something"
+
+    var twoWayText: String? = null
+    set(value) {
+        field = value
+        twoWayModifiedText = value?.chunked(1)?.joinToString("-") ?: ""
+        notifyChange()
+        //notifyPropertyChanged(BR.twoWayModifiedText)
+    }
 }
